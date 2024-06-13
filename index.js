cc8e8ff95894225197a6056ff909780fb2a0d219
HritikDutta <dutta.hritik@gmail.com>
2024-06-13 17:12:49 +0530

    FUCK THIS

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const header = document.getElementById('main-header');
-const content = document.getElementById('content');
+const everything = document.getElementById('everything');
 
 let headerOpaque = false;
 
 function setHeaderOpaqueness() {
-    if (window.scrollY >= 10 && !headerOpaque) {
+    if (everything.scrollTop >= 10 && !headerOpaque) {
         header.classList.add('header-opaque');
         headerOpaque = true;
-    } else if (window.scrollY < 10 && headerOpaque) {
+    } else if (everything.scrollTop < 10 && headerOpaque) {
         header.classList.remove('header-opaque');
         headerOpaque = false;
     }
 }
 
 function onLoad() {
-    document.addEventListener('scroll', setHeaderOpaqueness);
+    everything.addEventListener('scroll', setHeaderOpaqueness);
     setHeaderOpaqueness();
 }
